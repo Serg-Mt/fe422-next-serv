663d83ff84b5ac67b3d4202ba698190cc36f0607
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2024-11-30 12:37:35 +0400

    30 ноября середина

---
+++ components/todo/index.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import useSWR from 'swr';
+
+
+const
+  TOGGLE_CHECKBOX_ACTION = 'toggle-checkbox',
+  DELETE_ACTION = 'del',
+  ADD_ACTION = 'add',
+  DEL_COMPLETED_ACTION = 'del-completed',
+  API_URL = 'http://localhost:3333/todo',
+
+  fetcher = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('fetch ' + response.status);
+    return await response.json();
+  };
+
+
+export function ToDo() {
+  const
+    [text, setText] = useState(''),
+    { data, error, isLoading, isValidating, mutate } = useSWR(API_URL, fetcher),
+    onClick = async event => {
+      const
+        action = event.target.closest('[data-action]')?.dataset?.action,
+        id = event.target.closest('[data-id]')?.dataset?.id;
+      console.log('onClick', { action, id });
+      switch (action) {
+        case DELETE_ACTION:
+          await fetch(API_URL + '/' + id, { method: 'DELETE' });
+          await mutate();
+          return;
+        case ADD_ACTION:
+          await fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text, checked: false })
+          });
+          mutate();
+          return;
+        case TOGGLE_CHECKBOX_ACTION:
+          await fetch(API_URL + '/' + id, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ checked: !data.find(el => String(id) === String(el.id)).checked })
+          }
+          );
+          mutate();
+      };
+    };
+  console.log('swr=', { data, error });
+  return <fieldset onClick={onClick}>
+    <div style={{ position: 'absolute', fontSize: 'xxx-large' }}>
+      {isLoading && '⌛'}
+      {isValidating && '👁'}
+      {error && `💀 ${error.toString()}`}
+    </div>
+
+    <input value={text} onInput={event => setText(event.target.value)} />
+    <button data-action={ADD_ACTION}>add</button>
+    <ol>
+      {data?.map(item => <Item key={item.key} item={item} />)}
+    </ol>
+  </fieldset>;
+}
+
+function Item({ item }) {
+  const
+    { id, checked, text } = item;
+  return <li data-id={id}>
+    <input type="checkbox" checked={checked} data-action={TOGGLE_CHECKBOX_ACTION} />
+    <span>{text}</span>
+    <button data-action={DELETE_ACTION}>❌</button>
+    {checked && '💹'}
+  </li>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { ToDo } from '@/components/todo';
+
 
 export default function Home() {
   return <>
-
+    <h1>ToDo</h1>
+    <ToDo />
   </>;
 
 }
